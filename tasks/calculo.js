7ed0022d3c3a4df0114d8aad0f27384a5255f4d7
Raphael Rodrigues <rars.developer@gmail.com>
2023-10-20 11:43:08 -0300

    Implementando a função de movimento do Albert

--- tasks/calculo.js
+++ tasks/calculo.js
@@ -49,47 +49,43 @@ export const ativarCalculo = async () => {
       ]);
 
       const { num1, num2, operador } = answers;
-      const result = calculadora(
-        parseFloat(num1),
-        parseFloat(num2),
-        operador
-      );
+      const result = calculadora(parseFloat(num1), parseFloat(num2), operador);
       console.log("Resultado:", result);
     } catch (error) {
       console.error("Erro:", error);
     }
   };
 
+  function calculadora(n1, n2, op) {
+    switch (op) {
+      case "+":
+        return somar(n1, n2);
+      case "-":
+        return subtrair(n1, n2);
+      case "*":
+        return multiplicar(n1, n2);
+      case "/":
+        return dividir(n1, n2);
+      default:
+        return "Operação inválida";
+    }
+  }
+
+  function somar(n1, n2) {
+    return n1 + n2;
+  }
+
+  function subtrair(n1, n2) {
+    return n1 - n2;
+  }
+
+  function multiplicar(n1, n2) {
+    return n1 * n2;
+  }
+
+  function dividir(n1, n2) {
+    return n1 / n2;
+  }
+
   await operacao();
 };
-
-function calculadora(n1, n2, op) {
-  switch (op) {
-    case "+":
-      return somar(n1, n2);
-    case "-":
-      return subtrair(n1, n2);
-    case "*":
-      return multiplicar(n1, n2);
-    case "/":
-      return dividir(n1, n2);
-    default:
-      return "Operação inválida";
-  }
-}
-
-function somar(n1, n2) {
-  return n1 + n2;
-}
-
-function subtrair(n1, n2) {
-  return n1 - n2;
-}
-
-function multiplicar(n1, n2) {
-  return n1 * n2;
-}
-
-function dividir(n1, n2) {
-  return n1 / n2;
-}
--- tasks/movimento.js
+++ tasks/movimento.js
@@ -1,3 +1,85 @@
-export const ativarMovimento = () => {
-  console.log("Movimentar Albert");
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+export const ativarMovimento = async () => {
+  const movimentar = async () => {
+    try {
+      const answers = await promptMovimentar();
+
+      const { teclas } = answers;
+
+      const movements = {
+        w: moverParaCima,
+        s: moverParaBaixo,
+        a: moverParaEsquerda,
+        d: moverParaDireita,
+      };
+
+      if (movements.hasOwnProperty(teclas)) {
+        movements[teclas]();
+      } else {
+        console.log("Tecla inválida. Tente novamente.");
+      }
+    } catch (error) {
+      console.error("Erro:", error);
+    }
+  };
+
+  const promptMovimentar = () => {
+    return inquirer.prompt([
+      {
+        type: "list",
+        name: "teclas",
+        message: "Qual tecla deseja apertar?",
+        choices: [
+          {
+            value: "w", // Codigo para a tecla W
+            name: chalk.rgb(152, 3, 252)("⬆️ Cima"),
+          },
+          {
+            value: "s", // Codigo para a tecla S
+            name: chalk.rgb(3, 206, 252)("⬇️ Baixo"),
+          },
+          {
+            value: "a", // Codigo para a tecla A
+            name: chalk.rgb(107, 252, 3)("⬅️ Esquerda"),
+          },
+          {
+            value: "d", // Codigo para a tecla D
+            name: chalk.rgb(252, 3, 111)("➡️ Direita"),
+          },
+        ],
+        validate: input => {
+          if (
+            input === "w" ||
+            input === "s" ||
+            input === "a" ||
+            input === "d"
+          ) {
+            return true;
+          } else {
+            return "Tecla inválida. Tente novamente.";
+          }
+        },
+      },
+    ]);
+  };
+
+  function moverParaCima() {
+    console.log("Albert se moveu para cima!");
+  }
+
+  function moverParaBaixo() {
+    console.log("Albert se moveu para baixo!");
+  }
+
+  function moverParaEsquerda() {
+    console.log("Albert se moveu para esquerda!");
+  }
+
+  function moverParaDireita() {
+    console.log("Albert se moveu para direita!");
+  }
+
+  movimentar();
 };
